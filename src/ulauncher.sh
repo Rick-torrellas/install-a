254bca747cbabc2415a378a8517996860ce44ee8
Rick-torrellas <75484605+Rick-torrellas@users.noreply.github.com>
2021-11-02 10:38:31 -0400

    feat

--- src/ulauncher.sh
+++ src/ulauncher.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/bash
-wget https://github.com/Ulauncher/Ulauncher/releases/download/5.10.0/ulauncher_5.10.0_all.deb
-sudo apt install ./ulauncher_5.10.0_all.deb
-rm -f -r ./ulauncher_5.10.0_all.deb
+sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
